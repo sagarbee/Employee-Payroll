96713091fef70aff67a00b63c0de62c0c5077d0f
Sagar <Sagar@DESKTOP-QUVGL4M>
2023-03-07 00:08:53 +0530

    1]Designed the login page. 2]JDBC connectivity to 'epm' database(Mysql Workbench). 3]crated main.java file (set Visible after succefull login)

--- EmployeePayrollManagement/src/epm/login.java
+++ EmployeePayrollManagement/src/epm/login.java
@@ -1,41 +1,153 @@
 package epm;
 
-import java.awt.EventQueue;
+import javax.swing.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import epm.Main;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
 
 public class login extends JFrame {
 
-	private JPanel contentPane;
+    private JTextField txtusername;
+    private JPasswordField txtpassword;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					login frame = new login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+    public static void main(String[] args) {
+        new login();
+    
+    }
+
+    Connection con;
+	PreparedStatement pst;
+	ResultSet rs;
+	public void Connect()
+	{
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost/epm","root","Sagar@123");
+			
+		}
+		catch(ClassNotFoundException ex)
+		{
+			
+		}
+		catch(SQLException ex)
+		{
+			
+		}
 	}
+	
 
-	/**
-	 * Create the frame.
-	 */
-	public login() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    
+    public login() {
+    	super("login Form");
+        setSize(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
 
-		setContentPane(contentPane);
-	}
+        JPanel panel = new JPanel(new GridLayout(0, 2, 5, 5));
 
+        JLabel usernameLabel = new JLabel("Username:");
+        txtusername = new JTextField();
+        JLabel passwordLabel = new JLabel("Password:");
+        txtpassword = new JPasswordField();
+        
+    	Connect();
+
+        
+        JButton logininButton = new JButton("login");
+        logininButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	
+        		try {
+					Statement stmt = con.createStatement();
+					String sql = "select * from users where username = '"+txtusername.getText()+"' and password = '"+txtpassword.getText().toString()+"' ";
+					ResultSet rs = stmt.executeQuery(sql);
+					if(rs.next())
+					{
+						JOptionPane.showMessageDialog(null, "Login Successfull..!!","Login Success..!!",JOptionPane.INFORMATION_MESSAGE);
+						Main.main(null);
+						dispose();
+
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null, "Check The Credentials..!!");
+					}
+					con.close();
+        		}catch(Exception e1)
+        		{
+        			JOptionPane.showMessageDialog(null, "Database Error..!!", "Database Error", 2);
+        		}
+        	
+        	}
+        });
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	
+        		txtusername.setText(null);
+        		txtpassword.setText(null);
+        		
+        	}
+        });
+
+        panel.add(usernameLabel);
+        panel.add(txtusername);
+        panel.add(passwordLabel);
+        panel.add(txtpassword);
+        panel.add(logininButton);
+        panel.add(clearButton);
+
+        getContentPane().add(panel, BorderLayout.CENTER);
+
+        Color bgColor = new Color(37, 37, 37);
+        Color fgColor = Color.WHITE;
+        Color accentColor = new Color(90, 200, 250);
+
+        panel.setBackground(bgColor);
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 30, 20, 30));
+
+        Font font = new Font("Segoe UI", Font.PLAIN, 14);
+        usernameLabel.setFont(font);
+        usernameLabel.setForeground(fgColor);
+        passwordLabel.setFont(font);
+        passwordLabel.setForeground(fgColor);
+        txtusername.setFont(font);
+        txtpassword.setFont(font);
+        logininButton.setFont(font);
+        clearButton.setFont(font);
+
+        logininButton.setBackground(accentColor);
+        logininButton.setForeground(Color.WHITE);
+        logininButton.setBorderPainted(false);
+        logininButton.setFocusPainted(false);
+
+        clearButton.setBackground(bgColor);
+        clearButton.setForeground(fgColor);
+        clearButton.setBorder(BorderFactory.createLineBorder(accentColor));
+        clearButton.setFocusPainted(false);
+
+        txtusername.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, accentColor));
+        txtusername.setBackground(bgColor);
+        txtusername.setForeground(fgColor);
+        txtusername.setCaretColor(fgColor);
+
+        txtpassword.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, accentColor));
+        txtpassword.setBackground(bgColor);
+        txtpassword.setForeground(fgColor);
+        txtpassword.setCaretColor(fgColor);
+
+        setVisible(true);
+    }
 }
+
